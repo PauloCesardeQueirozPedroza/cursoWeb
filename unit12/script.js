30ad97c57ac8b46710a784fb0e02309738cc8d21
paulo <paulocesarpedroza2007@gmail.com>
2024-06-04 10:32:26 -0300

    POO

--- unit12/script.js
+++ unit12/script.js
@@ -115,7 +115,18 @@ function criarInputs(){
 }
 
 
+function calcularMedia() {
+    var inputs = document.getElementsByClassName("inputMedia")
+    var numeroInputs = inputs.length
 
+    var soma = 0
+
+    for(var i = 0; i < numeroInputs; i++){
+        soma += parseInt(inputs { i} .value)
+    }
+ document.getElementById("mediaFinal").innerHTML = soma / numeroInputs;
+
+}
 
 
 
---
+++ unit13/script.js
@@ -0,0 +1,47 @@
+//jquerry: biblioteca do JS que permite  simplificar a sintaxe
+
+//JavaScript
+//document.getElementById("nomeId");
+
+//JQuerry - Id
+//$("#nomeId");
+
+//JavaScript - Class
+//document.getElementsByClassName("nomeClasse")
+//Jquerry - tag
+//$("p")
+
+//seleciona todos os elementos de um HTML
+//$("*")
+//seleciona o primeiro elemento de <p></p>
+//$("p:first")
+//seleciona o ultimo elemento de <p></p>
+//$("p:last")
+//seleciona todos os elementos com o atributo disapled
+//$([disabled])
+
+//combinando seleções
+//JS
+//document.getElementById("nomeId").getElementsByTagName("p")
+//Q]
+//$("#nomeId p")
+
+//programação orientada a objeto
+
+//var nome = zezinha
+//var sobrenome = silva
+//var idade = 12
+//var pessoa = {nome: "zezinha", sobrenome: "silva", idade: 12}
+
+
+
+
+
+
+
+
+
+
+
+
+
